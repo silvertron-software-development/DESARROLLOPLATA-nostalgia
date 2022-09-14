b5e800ad90338a8bade4086798de315b78df8d1e
Diego <diegogplata@gmail.com>
2022-09-14 14:47:22 -0500

    initial commit complete site

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+var countDownDate = new Date("Apr 4, 2023 18:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the containers
+  document.getElementById("days-container").innerHTML = days + "d ";
+  document.getElementById("hours-container").innerHTML = hours + "h ";
+  document.getElementById("minutes-container").innerHTML = minutes + "m ";
+  document.getElementById("seconds-container").innerHTML = seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
